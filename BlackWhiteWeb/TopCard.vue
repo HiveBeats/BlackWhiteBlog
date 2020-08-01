c0ce9d7f8c8beca67b05a6655b1982924dd036a8
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2020-08-02 00:27:24 +0700

    add ui components basic

---
+++ BlackWhiteWeb/TopCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img class="top-card-img" :src="item.image" alt="" />
+    <p class="top-card-title">{{item.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopCard",
+  props: {
+    item: {
+      title:String,
+      image:String,
+      id: Number
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.top-card-title {
+  padding: 3px;
+}
+
+.top-card-img {
+  width: 100px;
+}
+
+</style>
