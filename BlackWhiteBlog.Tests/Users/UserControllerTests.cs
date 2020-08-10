7f406ed078c2a3f0650e58b2c61650bd918f1d99
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2020-08-10 11:18:17 +0700

    UserController Get Tests

--- BlackWhiteBlog.Tests/Users/UserControllerTests.cs
+++ BlackWhiteBlog.Tests/Users/UserControllerTests.cs
@@ -2,17 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
-using BlackWhiteBlog.Controllers;
 using BlackWhiteBlog.Controllers.Users;
 using System.Threading.Tasks;
 using BlackWhiteBlog.DbModels;
-using BlackWhiteBlog.Helpers.Permissions;
 using BlackWhiteBlog.Services;
 using BlackWhiteBlog.TransferObjects;
 using BlackWhiteBlog.TransferObjects.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Sqlite;
 
 namespace BlackWhiteBlog.Tests.Users
 {
@@ -45,6 +42,8 @@ namespace BlackWhiteBlog.Tests.Users
         }
     }
 
+    
+
     public class UserControllerTests : UsersTest
     {
         public UserControllerTests() : base(
@@ -53,6 +52,17 @@ namespace BlackWhiteBlog.Tests.Users
                 .Options)
         {
             
+        }
+
+
+        private User CreateTestUser(BlogDbContext ctx, int privs, int id)
+        {
+            var user = new User() {UserId = id, UserName = "John Gaven", Privs = privs, HashedPassword = "ndt5bm#gY"};
+            var author = new Author() {AuthorId = id, AuthorName = "Ваня Лалет", AuthorPicLink = "./pic", AuthorDesc = "Some Guy", UserId = id };
+            user.Author = author;
+            ctx.Users.Add(user);
+            ctx.SaveChanges();
+            return user;
         }
 
         [Fact]
@@ -70,6 +80,106 @@ namespace BlackWhiteBlog.Tests.Users
                 
                 Assert.Equal(1, items.Count());
                 Assert.Equal("John Grave", items.First().UserName);
+            }
+        }
+
+        [Fact]
+        public async Task Get_Items_BadRequest_IfNotValid()
+        {
+            using(var ctx = new BlogDbContext(ContextOptions))
+            {
+                var controller = new UserController(ctx, new UserService(ctx));
+
+                var actionResult = await controller.Get(null);
+                Assert.True(actionResult is BadRequestObjectResult);
+
+                actionResult = await controller.Get(new UserRequestDto(){UserId = 0, AuthorId = 1});
+                Assert.True(actionResult is BadRequestObjectResult);
+
+                actionResult = await controller.Get(new UserRequestDto(){UserId = null, AuthorId = 1});
+                Assert.True(actionResult is BadRequestObjectResult);
+            }
+        }
+
+        [Fact]
+        public async Task Get_Items_BadRequest_WithoutPrivs()
+        {
+            using (var ctx = new BlogDbContext(ContextOptions))
+            {
+                var controller = new UserController(ctx, new UserService(ctx));
+
+                var user = CreateTestUser(ctx, 1, 2);
+                var actionResult = await controller.Get(new UserRequestDto(){UserId = user.UserId, AuthorId = user.AuthorId});
+                Assert.True(actionResult is BadRequestObjectResult);
+
+                ctx.Users.Remove(user);
+                ctx.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task Can_Get_Item()
+        {
+            using (var ctx = new BlogDbContext(ContextOptions))
+            {
+                var controller = new UserController(ctx, new UserService(ctx));
+
+                var actionResult = await controller.Get(1, new UserRequestDto(){UserId = 1, AuthorId = 1});
+                Assert.True(actionResult is OkObjectResult);
+
+                var okResult = actionResult as OkObjectResult;
+                var item = okResult.Value as UserDetailDto;
+
+                Assert.Equal(1, item.UserId);
+                Assert.Equal("Ваня Лалетин", item.AuthorName);
+                Assert.Equal(4, item.Privs);
+                Assert.Equal("John Grave", item.UserName);
+            }
+        }
+
+        [Fact]
+        public async Task Get_Item_BadRequest_IfNotValid()
+        {
+            using(var ctx = new BlogDbContext(ContextOptions))
+            {
+                var controller = new UserController(ctx, new UserService(ctx));
+
+                var actionResult = await controller.Get(1, null);
+                Assert.True(actionResult is BadRequestObjectResult);
+
+                actionResult = await controller.Get(1, new UserRequestDto(){UserId = 0, AuthorId = 1});
+                Assert.True(actionResult is BadRequestObjectResult);
+
+                actionResult = await controller.Get(1, new UserRequestDto(){UserId = null, AuthorId = 1});
+                Assert.True(actionResult is BadRequestObjectResult);
+            }
+        }
+
+        [Fact]
+        public async Task Get_Item_BadRequest_WithoutPrivs()
+        {   
+            using (var ctx = new BlogDbContext(ContextOptions))
+            {
+                var controller = new UserController(ctx, new UserService(ctx));
+
+                var user = CreateTestUser(ctx, 1, 2);
+                var actionResult = await controller.Get(1, new UserRequestDto(){UserId = user.UserId, AuthorId = user.AuthorId});
+                Assert.True(actionResult is BadRequestObjectResult);
+
+                ctx.Users.Remove(user);
+                ctx.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task Get_Item_NotFound_IfNotExists()
+        {
+             using(var ctx = new BlogDbContext(ContextOptions))
+            {
+                var controller = new UserController(ctx, new UserService(ctx));
+
+                var actionResult = await controller.Get(4, new UserRequestDto(){UserId = 1, AuthorId = 1});
+                Assert.True(actionResult is NotFoundResult);
             }
         }
     
